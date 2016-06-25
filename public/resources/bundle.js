fc7ea1413848ddbb32300bb560feaa1953763d25
danielkermode <danielkermode@hotmail.co.nz>
2016-06-26 00:23:28 +1200

    loaded event listener

--- public/resources/bundle.js
+++ public/resources/bundle.js
@@ -20313,17 +20313,25 @@
 	  var img = new Image(); // Create new img element
 	  img.src = imgUrl;
 	  this.img = img;
-
-	  img.addEventListener("load", function () {
-	    // execute drawImage statements here
-	    // this.draw();
-	  }, false);
+	  this.loaded = false;
 	}
 
 	Hamster.prototype.draw = function (ctx) {
-	  ctx.font = "7px Georgia";
-	  ctx.fillText(this.name, this.x, this.y);
-	  ctx.drawImage(this.img, this.x, this.y);
+	  var _this = this;
+
+	  if (this.loaded) {
+	    ctx.font = "7px Georgia";
+	    ctx.fillText(this.name, this.x, this.y);
+	    ctx.drawImage(this.img, this.x, this.y);
+	  } else {
+	    this.img.addEventListener("load", function () {
+	      // execute drawImage statements here
+	      ctx.font = "7px Georgia";
+	      ctx.fillText(_this.name, _this.x, _this.y);
+	      ctx.drawImage(_this.img, _this.x, _this.y);
+	      _this.loaded = true;
+	    }, false);
+	  }
 	};
 
 	exports.default = Hamster;
