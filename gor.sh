cc215121e0794a562d9a7a8cae0a6afc7cb3ad80
danielkermode <danielkermode@hotmail.co.nz>
2016-07-06 23:33:45 +1200

    reldir

--- gor.sh
+++ gor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkill -f criceti
-cp -r ~/Desktop/code/gocode/src/github.com/danielkermode/criceti/chat/ ~/Desktop/code/gocode/src/github.com/danielkermode/criceti/vendor/github.com/danielkermode/criceti/
+cp -r ./chat/ ./vendor/github.com/danielkermode/criceti/
 go install
 criceti &
 inotifywait -e close_write,moved_to,create  . |
