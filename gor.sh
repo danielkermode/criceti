c37c2e64c421f5b23e0123dd73e13fafeb331508
danielkermode <danielkermode@hotmail.co.nz>
2016-07-06 23:39:03 +1200

    watchchatdir

--- gor.sh
+++ gor.sh
@@ -3,7 +3,7 @@ pkill -f criceti
 cp -r ./chat/ ./vendor/github.com/danielkermode/criceti/
 go install
 criceti &
-inotifywait -e close_write,moved_to,create  . |
+inotifywait -e close_write,moved_to,create  . ./chat |
 while read -r directory events filename; do
   ./gor.sh
 done
