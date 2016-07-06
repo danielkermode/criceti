dd171dabc070ebe567df21872c5808d64a5290a5
danielkermode <danielkermode@hotmail.co.nz>
2016-07-06 23:32:09 +1200

    watchscript

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = "5000"
 	}
 	fmt.Println("listening on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
