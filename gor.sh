9d20dfbd67cab30539030da084aa723b869983b9
danielkermode <danielkermode@hotmail.co.nz>
2016-07-06 23:28:18 +1200

    watchscript

--- gor.sh
+++ gor.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+pkill -f criceti
 cp -r ~/Desktop/code/gocode/src/github.com/danielkermode/criceti/chat/ ~/Desktop/code/gocode/src/github.com/danielkermode/criceti/vendor/github.com/danielkermode/criceti/
-git add .
-git commit -m $1
-git push
-govendor update github.com/danielkermode/criceti/chat
+go install
+criceti &
+inotifywait -e close_write,moved_to,create  . |
+while read -r directory events filename; do
+  ./gor.sh
+done
